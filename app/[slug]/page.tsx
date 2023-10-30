27da8fa391f544162f0ee21bda12da3628b994a7
restreposanti <santirstrp@gmail.com>
2023-10-30 10:16:34 -0500

    feat: live preview working locally

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,10 +1,11 @@
-// ./nextjs-app/app/[slug]/page.tsx
-
 import { SanityDocument } from "@sanity/client";
-import Post from "@/components/Post";
+import { draftMode } from "next/headers";
+import Post from "@/components/Post/Post";
 import { postPathsQuery, postQuery } from "@/sanity/lib/queries";
-import { sanityFetch } from "@/sanity/lib/sanityFetch";
+import { sanityFetch, token } from "@/sanity/lib/sanityFetch";
 import { client } from "@/sanity/lib/client";
+import PreviewProvider from "@/components/PreviewProvider/PreviewProvider";
+import PreviewPost from "@/components/PreviewPost/PreviewPost";
 
 // Prepare Next.js to know which routes already exist
 export async function generateStaticParams() {
@@ -16,6 +17,15 @@ export async function generateStaticParams() {
 
 export default async function Page({ params }: { params: any }) {
   const post = await sanityFetch<SanityDocument>({ query: postQuery, params });
+  const isDraftMode = draftMode().isEnabled;
+
+  if (isDraftMode && token) {
+    return (
+      <PreviewProvider token={token}>
+        <PreviewPost post={post} />
+      </PreviewProvider>
+    );
+  }
 
   return <Post post={post} />;
 }
